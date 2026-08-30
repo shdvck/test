f55b5baf6847cfb6ad3ae4310d13ec1c46827283
shdvck <1039481978@qq.com>
2026-08-30 23:11:26 +0800

    报错

--- t2.py
+++ t2.py
@@ -6,8 +6,3 @@ guess = 0
 while guess != secret_number:
     guess = int(input("猜一个 1 到 10 之间的数字："))
     if guess < secret_number:
-        print("猜小了，再试试！")
-    elif guess > secret_number:
-        print("猜大了，再试试！")
-
-print("恭喜你，猜对啦！")
